acb05f348c3c8eff8a9b06cf63c5ecc506a9068c
ordon <ordon1313@gmail.com>
2015-12-06 15:52:14 +0100

    very tiny optimization

--- src/main/kotlin/crosswordsads/Words.kt
+++ src/main/kotlin/crosswordsads/Words.kt
@@ -2,9 +2,18 @@ package crosswordsads
 
 import java.io.File
 
-class Words(val listOfStrings: List<String>) {
+class Words {
+
+    val listOfStrings: Set<String>
+    val letterToWords: Map<Char, List<String>>
+
+    constructor(listOfStrings: List<String>) {
+        this.listOfStrings = listOfStrings.toSet()
+        letterToWords = listOfStrings.groupBy { it.first() }
+    }
+
     fun find(s: String): Boolean {
-        return listOfStrings.any { it.startsWith(s) }
+        return letterToWords[s.first()]!!.any { it.startsWith(s) }
     }
 
     fun isWord(s: String): Boolean {
