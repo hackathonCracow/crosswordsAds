78f955af7ed789620c148017bf888977c426d0a9
ghostbuster91 <kghost0@gmail.com>
2015-12-06 11:23:06 +0100

    get them all :)

--- dictionary/get.sh
+++ dictionary/get.sh
@@ -2,5 +2,5 @@
 for i in {1..2899}
 do
    echo "Welcome $i times"
-   wget -q -S -O page_$i http://sjp.pl/slownik/lp.phtml?f_vl=2&page=$i
+   curl "http://sjp.pl/slownik/lp.phtml?f_vl=2&page=$i" > page_$i
 done
