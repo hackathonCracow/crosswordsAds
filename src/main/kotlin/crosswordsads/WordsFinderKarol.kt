3ddc329a22d4bbd6467a68e8bbde6abe4ccf499e
ordon <ordon1313@gmail.com>
2015-12-06 14:56:34 +0100

    speed up after benchmark

--- src/main/kotlin/crosswordsads/WordsFinderKarol.kt
+++ src/main/kotlin/crosswordsads/WordsFinderKarol.kt
@@ -5,28 +5,36 @@ fun getWordsOnBoardKarol(board: Board, words: Words): List<List<String>> {
 }
 
 fun goDeper(proposal: List<Int>, board: Board, words: Words): Collection<List<String>> {
-    if (hasDuplicates(proposal)) {
-        return emptyList()
+    val stringFromIds = board.getStringFromIds(proposal)
+    val (exists, startsWith) = words.find(stringFromIds)
+    val current = if (exists) {
+        println(stringFromIds)
+        listOf(proposal.map { it.toString() })
     } else {
-        val stringFromIds = board.getStringFromIds(proposal)
-        val (exists, startsWith) = words.find(stringFromIds)
-        val current = if (exists) {
-            println(stringFromIds)
-            listOf(proposal.map { it.toString() })
-        } else {
-            emptyList()
-        }
-        val more = if (startsWith) {
-            (0..15).filter { areClose(it, proposal.last()) }.flatMap { goDeper(proposal + it, board, words) }
-        } else {
-            emptyList()
-        }
-        return current + more
+        emptyList()
     }
+    val more = if (startsWith) {
+        neighbours[proposal.last()]!!.filter { !proposal.contains(it) }.flatMap { goDeper(proposal + it, board, words) }
+    } else {
+        emptyList()
+    }
+    return current + more
 }
 
-fun hasDuplicates(proposal: List<Int>) = proposal.distinct().size != proposal.size
-
-fun areClose(it: Int, last: Int): Boolean {
-    return listOf(1, 4, 5, -1, -4, -5, -3, 3).contains(it - last)
-}
+private val neighbours = mapOf(
+        0 to listOf(1, 4, 5),
+        1 to listOf(0, 4, 5, 6, 2),
+        2 to listOf(1, 5, 6, 7, 3),
+        3 to listOf(2, 6, 7),
+        4 to listOf(0, 1, 5, 8, 9),
+        5 to listOf(0, 1, 2, 4, 6, 8, 9, 10),
+        6 to listOf(1, 2, 3, 5, 7, 9, 10, 11),
+        7 to listOf(2, 3, 6, 10, 11),
+        8 to listOf(4, 5, 9, 12, 13),
+        9 to listOf(4, 5, 6, 8, 10, 12, 13, 14),
+        10 to listOf(5, 6, 7, 9, 11, 13, 14, 15),
+        11 to listOf(6, 7, 10, 14, 15),
+        12 to listOf(8, 9, 13),
+        13 to listOf(8, 9, 10, 12, 14),
+        14 to listOf(9, 10, 11, 13, 15),
+        15 to listOf(10, 11, 14))
