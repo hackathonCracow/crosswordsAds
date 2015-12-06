281c725f0660242d6119c0193169fc9ef58c014e
ordon <ordon1313@gmail.com>
2015-12-06 11:28:13 +0100

    return smth so I can go on

--- src/main/kotlin/crosswordsads/WordsFinder.kt
+++ src/main/kotlin/crosswordsads/WordsFinder.kt
@@ -1,5 +1,5 @@
 package crosswordsads
 
-fun getWordsOnBoard(board: Board, words: Words) {
-
+fun getWordsOnBoard(board: Board, words: Words): List<List<String>> {
+    return listOf(listOf("0", "1", "2", "3", "6", "9", "12", "13", "14", "15"))
 }
