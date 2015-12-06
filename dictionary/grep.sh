bd19b7c8a823feeb04df00ad3c72fa6d68681264
ghostbuster91 <kghost0@gmail.com>
2015-12-06 11:34:39 +0100

    fix grep sh

--- dictionary/grep.sh
+++ dictionary/grep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..2899}
 do
-   cat page_1 | grep "</a></td>" | cut -d"=" -f2 | cut -d'>' -f2 | cut -d "<" -f1
+   cat page_$i | grep "</a></td>" | cut -d"=" -f2 | cut -d'>' -f2 | cut -d "<" -f1
 done
