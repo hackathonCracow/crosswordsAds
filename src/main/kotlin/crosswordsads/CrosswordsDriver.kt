fe2e8c815b23debb002be4182e72d0304bec6f8d
ordon <ordon1313@gmail.com>
2015-12-06 11:00:02 +0100

    register

--- src/main/kotlin/crosswordsads/CrosswordsDriver.kt
+++ src/main/kotlin/crosswordsads/CrosswordsDriver.kt
@@ -4,12 +4,17 @@ import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 
 class SeleniumCrosswordsDriver(val webDriver: WebDriver) : CrosswordsDriver {
+
     override fun typeInId(id: String, text: String) {
         webDriver.findElement(By.id(id)).sendKeys(text)
     }
 
+    override fun clickOnXpath(type: String) {
+        webDriver.findElement(By.xpath(type)).click()
+    }
 }
 
 interface CrosswordsDriver {
     fun typeInId(id: String, text: String)
+    fun clickOnXpath(type: String)
 }
--- src/main/kotlin/crosswordsads/Login.kt
+++ src/main/kotlin/crosswordsads/Login.kt
@@ -3,4 +3,12 @@ package crosswordsads
 fun login(crosswordsDriver: CrosswordsDriver) {
     crosswordsDriver.typeInId("Firstname", "Maski");
     crosswordsDriver.typeInId("Lastname", "Przeciwpyłowe")
+    crosswordsDriver.typeInId("Email", randomEmail())
+    crosswordsDriver.typeInId("Password", "123456")
+    crosswordsDriver.typeInId("ConfirmPassword", "123456")
+    crosswordsDriver.clickOnXpath("//input[@type=\"submit\"]")
+}
+
+fun randomEmail(): String {
+    return "uniqueemail@shitmail.me"
 }
